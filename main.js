2ac6efd9002c5493db5cd48fa144e35bf93a95c5
shinkureru <zhen0564115@gmail.com>
2024-10-27 18:29:40 +0800

    1027FinishProject

--- main.js
+++ main.js
@@ -209,6 +209,15 @@ citySelect.addEventListener('change', function() {
     }
 });
 
+//-----------------------------固定懸浮在畫面範圍的下方
+
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'  // 平滑滾動到最頂部
+    });
+  });
+
 
 //-----------------------------表單驗證系統
 
@@ -244,3 +253,4 @@ citySelect.addEventListener('change', function() {
       this.style.background = `linear-gradient(to right, rgba(118, 126, 50, 1) ${value}%, #ffffff ${value}%)`;
     });
   });
+
